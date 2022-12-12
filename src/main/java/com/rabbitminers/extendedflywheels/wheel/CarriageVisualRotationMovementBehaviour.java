09ef23357bf8b3e4b52abcb87989a09ffec67a43
Charlie Wood <rabbitminers2.0@gmail.com>
2022-12-12 13:27:42 +0000

    Fixed Rotation On Contraptions

--- src/main/java/com/rabbitminers/extendedflywheels/wheel/CarriageVisualRotationMovementBehaviour.java
+++ src/main/java/com/rabbitminers/extendedflywheels/wheel/CarriageVisualRotationMovementBehaviour.java
@@ -7,6 +7,7 @@ import com.simibubi.create.content.contraptions.components.structureMovement.ren
 import com.simibubi.create.content.logistics.trains.entity.CarriageContraption;
 import com.simibubi.create.content.logistics.trains.entity.CarriageContraptionEntity;
 import com.simibubi.create.foundation.utility.VecHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -43,12 +44,20 @@ public class CarriageVisualRotationMovementBehaviour implements MovementBehaviou
             if (!(tes.get(context.localPos) instanceof IVisualRotationWheel speedForcible))
                 return;
 
+            if (!(context.contraption instanceof CarriageContraption carriageContraption))
+                return;
+
+            if (Minecraft.getInstance().isPaused())
+                return;
+
+            Direction direction = carriageContraption.getAssemblyDirection();
+
             double distanceTo = 0;
             if (!cce.firstPositionUpdate) {
                 Vec3 diff = context.motion;
                 Vec3 relativeDiff = VecHelper.rotate(diff, cce.yaw, Direction.Axis.Y);
                 double signum = Math.signum(-relativeDiff.x);
-                distanceTo = diff.length() * signum/3.0f;
+                distanceTo = diff.length() * signum/3f;
             }
 
             double wheelRadius = speedForcible.getWheelRadius();
@@ -59,7 +68,10 @@ public class CarriageVisualRotationMovementBehaviour implements MovementBehaviou
             //Now figure out speed to achieve this
             float speed = (float) (angleDiff * 10 / 3f);
             //speedForcible.setForcedSpeed((float) angleDiff * 3 / 10f);
-            speedForcible.setAngle((float) ((speedForcible.getAngle() + (angleDiff * 3 / 10f)) % 360));
+            switch (direction) {
+                case NORTH, EAST -> speedForcible.setAngle((float) ((speedForcible.getAngle() + (angleDiff * 3 / 10f)) % 360));
+                case SOUTH, WEST -> speedForcible.setAngle((float) ((speedForcible.getAngle() - (angleDiff * 3 / 10f)) % 360));
+            }
         }
     }
     /*
