4d3b5ad25e159cd39ea422bee981c0e16acb8d31
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-19 23:13:17 +0000

    Added Rotation Types As Enum

---
+++ common/src/main/java/com/rabbitminers/extendedflywheels/base/FlywheelRotationType.java
@@ -0,0 +1,30 @@
+package com.rabbitminers.extendedflywheels.base;
+
+import com.simibubi.create.foundation.gui.AllIcons;
+import com.simibubi.create.foundation.tileEntity.behaviour.scrollvalue.INamedIconOptions;
+import com.simibubi.create.foundation.utility.Lang;
+
+public enum FlywheelRotationType implements INamedIconOptions {
+    DRIVE(AllIcons.I_REFRESH), TRAIL(AllIcons.I_ROTATE_CCW), STATIONARY(AllIcons.I_BLACKLIST),
+
+    ;
+
+    private String translationKey;
+    private AllIcons icon;
+
+    private FlywheelRotationType(AllIcons icon) {
+        this.icon = icon;
+        translationKey = "generic." + Lang.asId(name());
+    }
+
+    @Override
+    public AllIcons getIcon() {
+        return icon;
+    }
+
+    @Override
+    public String getTranslationKey() {
+        return translationKey;
+    }
+
+}
