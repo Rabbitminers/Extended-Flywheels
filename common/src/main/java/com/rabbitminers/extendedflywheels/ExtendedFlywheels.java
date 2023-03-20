39c561a1eac4c4f300a3ed8554dda37495d4dcdd
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-20 17:33:13 +0000

    Migrated Registrate To Entrypoint

--- common/src/main/java/com/rabbitminers/extendedflywheels/ExtendedFlywheels.java
+++ common/src/main/java/com/rabbitminers/extendedflywheels/ExtendedFlywheels.java
@@ -1,6 +1,9 @@
 package com.rabbitminers.extendedflywheels;
 
 import com.rabbitminers.extendedflywheels.registry.ExtendedFlywheelsBlocks;
+import com.rabbitminers.extendedflywheels.registry.ExtendedFlywheelsItems;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,12 +13,23 @@ public class ExtendedFlywheels {
     public static final String NAME = "Extended Flywheels";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
+    private static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ExtendedFlywheels.MOD_ID)
+            .creativeModeTab(() -> ExtendedFlywheelsItems.itemGroup);
 
     public static void init() {
         ExtendedFlywheelsBlocks.init();
     }
 
-    public static ResourceLocation id(String path) {
+    public static ResourceLocation asResource(String path) {
         return new ResourceLocation(MOD_ID, path);
+    }
+
+    public static CreateRegistrate registrate() {
+        return REGISTRATE;
+    }
+
+    @ExpectPlatform
+    public static int getNextAvailableTabId() {
+        throw new AssertionError();
     }
 }
